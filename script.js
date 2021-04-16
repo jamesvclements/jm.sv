55712e3473d6dcb22609eefd1cb604fcb94a9d65
James Clements <jamesvclements@gmail.com>
2021-04-16 16:22:54 -0400

    flush

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const tag = document.createElement('div');
+    tag.classList.add('background');
+    document.getElementsByTagName('body')[0].appendChild(tag);
+});
